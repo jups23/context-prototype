e9ba1129effb0735b0533790b32eb370f5023aeb
Willi <willi@jups42.de>
2014-05-14 00:15:37 -0300

    add server that echos POST/GET params

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def hello_world():
+    print request.form
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
