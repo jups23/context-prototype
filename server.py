193e4f929bf8cb3fb7ddf386b77eb7407a81f2c8
Willi <willi@jups42.de>
2014-05-23 17:57:32 -0300

    python Flask filters and pipes stuff into processing

--- server.py
+++ server.py
@@ -1,12 +1,22 @@
+import socket
+
 from flask import Flask
 from flask import request
 
 app = Flask(__name__)
 
+UDP_IP = "localhost"
+UDP_PORT = 6000
+sock = socket.socket(socket.AF_INET, # Internet
+             socket.SOCK_DGRAM) # UDP
+
+
 @app.route('/', methods=["GET", "POST"])
 def hello_world():
-    print request.form
-    return "Hello World!"
+    x = request.form.get('acceleration.x')
+    print x
+    sock.sendto(str(x), (UDP_IP, UDP_PORT))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False)
