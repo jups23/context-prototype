ebb3287c3f4f98ecbaeca77011cbc1173d3532de
Willi <willi@jups42.de>
2014-05-27 11:11:29 -0300

    pong with 'shake to invoke', python sends JSON to processing

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import pdb
+import json
 import socket
 
 from flask import Flask
@@ -11,14 +12,9 @@ UDP_PORT = 6000
 sock = socket.socket(socket.AF_INET, # Internet
              socket.SOCK_DGRAM) # UDP
 
-
 @app.route('/', methods=["GET", "POST"])
 def hello_world():
-    data = float(request.form.get('attitudePitch'))
-    if abs(data):
-        print data
-        sock.sendto(str(data), (UDP_IP, UDP_PORT))
-
+    sock.sendto(json.dumps(request.form), (UDP_IP, UDP_PORT))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False)
